ff583cb1f428a64b64ee543e795fb3855f7a53c7
hrzaman <148742224+hrzaman@users.noreply.github.com>
2023-10-24 11:08:14 +0330

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,6 +1,6 @@
 // 0.5.1-c8a2
 // Enable optimization
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.18;
 
 import "./TRC20.sol";
 import "./TRC20Detailed.sol";
@@ -16,7 +16,7 @@ contract Token is TRC20, TRC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public TRC20Detailed("YourTokenName", "YTN", 18) {
-        _mint(msg.sender, 10000000000 * (10 ** uint256(decimals())));
+    constructor () public TRC20Detailed("Global", "GBL", 18) {
+        _mint(msg.sender, 1000000000000 * (10 ** uint256(decimals())));
     }
 }
